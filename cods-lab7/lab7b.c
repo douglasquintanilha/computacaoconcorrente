6773da5de8c8420562218c4f3c0aff57f452e9aa
Douglas Quintanilha <dougquintanilha@gmail.com>
2015-11-26 17:23:08 -0200

    comecando parte b

--- cods-lab7/lab7b.c
+++ cods-lab7/lab7b.c
@@ -8,13 +8,32 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define NTHREADS  4
+#define NTHREADS  5
 #define PASSOS 5
 
 /* Variaveis globais */
 int x = 0;
 pthread_mutex_t x_mutex;
 pthread_cond_t x_cond;
+
+void barreira(int nthreads){
+
+}
+
+void *A (void *arg) {
+    int tid = * (int*)arg, i;
+    int cont = 0, boba1, boba2;
+    
+    for (i=0; i < PASSOS; i++) {
+        cont++;
+        printf("Thread %d: cont=%d, passo=%d\n", tid, cont, i);
+        //sincronizacao condicional
+        barreira(NTHREADS);
+        /*faz alguma coisa inutil pra gastar tempo...*/
+        boba1=100; boba2=-100; while (boba2 < boba1) boba2++;
+    }
+pthread_exit(NULL);
+}
 
 
 /* Funcao principal */
@@ -27,11 +46,13 @@ int main(int argc, char *argv[]) {
     pthread_cond_init (&x_cond, NULL);
 
     /* Cria as threads */
-    pthread_create(&threads[2], NULL, C, NULL);
-    pthread_create(&threads[3], NULL, D, NULL);
+    
     pthread_create(&threads[0], NULL, A, NULL);
-    pthread_create(&threads[1], NULL, B, NULL);
-
+    pthread_create(&threads[1], NULL, A, NULL);
+    pthread_create(&threads[2], NULL, A, NULL); 
+    pthread_create(&threads[3], NULL, A, NULL);
+    pthread_create(&threads[4], NULL, A, NULL);
+    
     /* Espera todas as threads completarem */
     for (i = 0; i < NTHREADS; i++) {
     pthread_join(threads[i], NULL);
