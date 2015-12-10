1c7f97f63fbe0a7857f927e60b6a0a47fb305e96
Douglas Quintanilha <dougquintanilha@gmail.com>
2015-12-10 09:53:24 -0200

    Comentario de como fazer a atividade b

---
+++ cods-lab9/lab9b.c
@@ -0,0 +1,3 @@
+// É como se fosse o reverso do que foi pedido na atividade 1.
+// Agora temos que criar uma nova variavel de condição pra sinalizar uma "fila" de escritores que querem escrever, e só quando esta 
+// fila for zerado, é que um leitor pode ler. É um starvation dos leitores agora. (Antes era dos escritores).
