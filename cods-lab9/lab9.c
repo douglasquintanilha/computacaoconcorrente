8def11ec632fa0d9f2e3fef254f8e4f0d3948eb2
Douglas Quintanilha <dougquintanilha@gmail.com>
2015-12-10 09:43:57 -0200

    Primeira parta do Laboratorio de Comp Conc

---
+++ cods-lab9/lab9.c
@@ -0,0 +1,133 @@
+/* Disciplina: Computacao Concorrente */
+/* Prof.: Silvana Rossetto */
+/* Aluno: Douglas Quintanilha Barbosa Ferreira */
+/* DRE: 112012763 */
+/* Laboratório: 8 */
+
+#include <pthread.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+#define N 5 // LEITORES 
+#define M 2 // ESCRITORES
+
+pthread_mutex_t mutex;
+pthread_cond_t cond_leitor;
+pthread_cond_t cond_escritor;
+
+int escritores = 0;
+int leitores = 0;
+
+typedef struct a
+{
+	int contador;
+	int idThread;
+}Arquivo;
+
+Arquivo arquivo;
+
+void inicializa_leitor(){
+	pthread_mutex_lock(&mutex);
+	while(escritores > 0){
+		pthread_cond_wait(&cond_leitor, &mutex);
+	}
+	leitores++;
+	pthread_mutex_unlock(&mutex);
+}
+
+void finaliza_leitor(){
+	pthread_mutex_lock(&mutex);
+	leitores --;
+	if(leitores == 0 )
+		pthread_cond_signal(&cond_escritor);
+	pthread_mutex_unlock(&mutex);
+}
+void inicializa_escritor(){
+	pthread_mutex_lock(&mutex);
+	while( (leitores > 0) || (escritores > 0)){
+		pthread_cond_wait(&cond_escritor,&mutex);
+	}
+	escritores ++;
+	pthread_mutex_unlock(&mutex);
+}
+
+void finaliza_escritor(){
+	pthread_mutex_lock(&mutex);
+	escritores --;
+	pthread_cond_signal(&cond_escritor);
+	pthread_cond_broadcast(&cond_leitor);
+	pthread_mutex_unlock(&mutex);
+}
+
+void * leitor(void * arg){
+	int* p = (int *) arg;
+	int pid =  (int) *p;
+	Arquivo arquivoLocal;
+	
+	while(1){
+		printf("Thread Leitora[%d]\n", pid);
+		inicializa_leitor();	
+		//Lê
+		arquivoLocal.contador = arquivo.contador;
+		arquivoLocal.idThread = arquivo.idThread;
+		printf("Leu o contador: [%d] que foi gravado pela Thread: [%d] \n",arquivoLocal.contador,arquivoLocal.idThread );
+		finaliza_leitor();
+		sleep(1);
+	}
+	pthread_exit(NULL);
+}
+
+void* escritor(void *arg){
+	int * p = (int *) arg;
+	int pid = (int) *p;
+	while(1){
+		printf("Thread Escritora[%d]\n", pid);
+		inicializa_escritor();
+		// Escreve
+		pthread_mutex_lock(&mutex);
+		arquivo.contador ++;
+		arquivo.idThread = pid;
+		pthread_mutex_unlock(&mutex);
+
+		finaliza_escritor();
+		sleep(1);
+	}
+	pthread_exit(NULL);
+}
+
+int main(int argc, char *argv[]){
+	int i,j;
+	int* pid;
+    pthread_t threads[N+M];
+	
+	// Inicializa Threads Escritoras
+    for ( i = 0; i < M; ++i)
+    {
+    	pid = (int*) malloc(sizeof(int));
+    	*pid = i;
+		pthread_create(&threads[i],NULL, escritor,(void *) pid);
+    }
+
+    // Inicializa Threads Leitoras
+    for ( j = M; j < N; ++j)
+    {
+    	pid = (int*) malloc(sizeof(int));
+    	*pid = j;
+    	pthread_create(&threads[j],NULL, leitor,(void *) pid);
+    }
+
+
+    for (i = 0; i < N + M; ++i)
+    {
+        pthread_join(threads[i],NULL);
+    }
+
+	pthread_mutex_destroy(&mutex);
+    pthread_cond_destroy(&cond_leitor);
+	pthread_cond_destroy(&cond_escritor);
+	free(pid);
+
+   return 0;
+}
+
